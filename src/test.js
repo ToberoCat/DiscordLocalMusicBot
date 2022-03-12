52e1edfa6c4066039a8a02e97a6952bd69a48f1a
Tobero <67831042+ToberoCat@users.noreply.github.com>
2022-03-12 19:59:43 +0100

    Delete test.js

--- src/test.js
+++
@@ -1,5 +0,0 @@
-const { MessageActionRow, MessageButton, MessageEmbed} = require('discord.js');
-
-const embed = new MessageEmbed().setTimestamp().setImage("http://img.youtube.com/vi/bESGLojNYSo/hqdefault.jpg");
-
-console.log(embed.toJSON())
